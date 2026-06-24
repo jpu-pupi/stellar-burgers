f9604253fbed86ced7e9ef0e2d0dc1e1c139e772
Tank <soboleva.tan11@gmail.com>
2026-06-24 15:26:19 +0300

    доработала хедер и логин

--- src/components/ui/app-header/app-header.tsx
+++ src/components/ui/app-header/app-header.tsx
@@ -14,7 +14,12 @@ export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => (
     <nav className={`${styles.menu} p-4`}>
       <div className={styles.menu_part_left}>
         <>
-          <NavLink to='/' className={styles.link}>
+          <NavLink
+            to='/'
+            className={({ isActive }) =>
+              `${styles.link} ${isActive ? styles.link_active : ''}`
+            }
+          >
             <BurgerIcon type={'primary'} />
             <p className='text text_type_main-default ml-2 mr-10'>
               Конструктор
@@ -22,7 +27,12 @@ export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => (
           </NavLink>
         </>
         <>
-          <NavLink to='/feed' className={styles.link}>
+          <NavLink
+            to='/feed'
+            className={({ isActive }) =>
+              `${styles.link} ${isActive ? styles.link_active : ''}`
+            }
+          >
             <ListIcon type={'primary'} />
             <p className='text text_type_main-default ml-2'>Лента заказов</p>
           </NavLink>
@@ -32,7 +42,12 @@ export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => (
         <Logo className='' />
       </div>
       <div className={styles.link_position_last}>
-        <NavLink to='/profile' className={styles.link}>
+        <NavLink
+          to='/profile'
+          className={({ isActive }) =>
+            `${styles.link} ${isActive ? styles.link_active : ''}`
+          }
+        >
           <ProfileIcon type={'primary'} />
           <p className='text text_type_main-default ml-2'>
             {userName || 'Личный кабинет'}
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -2,22 +2,16 @@ import { FC, SyntheticEvent, useState } from 'react';
 import { LoginUI } from '@ui-pages';
 import { useDispatch } from '../../services/store';
 import { loginUser } from '../../services/slices/userSlice';
-import { useNavigate } from 'react-router-dom';
 
 export const Login: FC = () => {
   const dispatch = useDispatch();
-  const navigate = useNavigate();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
 
-    dispatch(loginUser({ email, password }))
-      .unwrap()
-      .then(() => {
-        navigate('/profile', { replace: true });
-      });
+    dispatch(loginUser({ email, password }));
   };
 
   return (
